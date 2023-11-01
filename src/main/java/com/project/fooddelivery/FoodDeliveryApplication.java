d6c321a3ad6456c8763e6ed3f6f05ba9e0b26a3c
nsahloum <sahloumi.najima@gmail.com>
2023-11-01 21:37:28 +0100

    eureka server

--- src/main/java/com/project/fooddelivery/FoodDeliveryApplication.java
+++ src/main/java/com/project/fooddelivery/FoodDeliveryApplication.java
@@ -2,8 +2,10 @@ package com.project.fooddelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FoodDeliveryApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/project/fooddelivery/FoodDeliveryApplicationTests.java
+++ src/test/java/com/project/fooddelivery/FoodDeliveryApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoodDeliveryApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
